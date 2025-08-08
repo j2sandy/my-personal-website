c9820f1feaa136c69fac9b525cbddf3768c9ca89
j2sandy <36033311+j2sandy@users.noreply.github.com>
2025-08-08 22:06:03 +0530

    Create Typewriter text.jsx

---
+++ src/pages/Typewriter text.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+export default function TypewriterText({ text = "", speed = 30 }) {
+  const [displayed, setDisplayed] = useState("");
+
+  useEffect(() => {
+    setDisplayed("");
+    let i = 0;
+    const interval = setInterval(() => {
+      if (i < text.length) {
+        setDisplayed((prev) => prev + text.charAt(i));
+        i++;
+      } else {
+        clearInterval(interval);
+      }
+    }, speed);
+    return () => clearInterval(interval);
+  }, [text]);
+
+  return <p className="text-center text-xl leading-relaxed">{displayed}</p>;
+}
