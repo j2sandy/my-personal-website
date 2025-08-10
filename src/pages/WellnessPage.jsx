6ac7055084b2a8392fb85cf5c2916eeb04654a0c
j2sandy <36033311+j2sandy@users.noreply.github.com>
2025-08-10 19:39:56 +0530

    Update WellnessPage.jsx

--- src/pages/WellnessPage.jsx
+++ src/pages/WellnessPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import CalendarPage from './CalendarPage';
+import CalendarPage from '../wellness/CalendarPage';
 import { wellnessData } from '../data/wellnessData';
 
 const tabs = [
